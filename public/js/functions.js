a8dd339cc9c08475864b9bb09c2e3a96a416d0b7
suadah <suadahaji@gmail.com>
2015-11-27 04:37:00 +0300

    Changed Input fields and submit button

--- public/js/functions.js
+++ public/js/functions.js
@@ -3,7 +3,7 @@ var Ref = new Firebase("https://easyattendance.firebaseio.com");
 var Events = Ref.child("events");
 var eventId;
 //login the users
-$('#members').on("click", function() {
+$('#log-in').on("click", function() {
   login();
 });
 
@@ -18,53 +18,50 @@ function login() {
   });
 }
 
-// checks if user is logged in
-function logged_in() {
-  var authData = Ref.getAuth();
-  if (authData) {
-
-    //return authData.google.displayName;
-    // console.log(authData)
-    //console.log("User " + authData.uid + " is logged in with " + authData.provider);
-  } else {
-    // console.log("User is logged out");
-  }
-}
-
-// if (logged_in()) {
-//   var authData = Ref.getAuth();
-//   var uid = Ref.child("users");
-
-//   uid.push().set({
-//     user_name: authData.google.displayName
-//   });
-
-// }
-
-// Displaying events on the table
-Events.on("child_added", function(snap) {
-  var events = snap.val();
-  // console.log("all events", snap.key(), snap.val());
-  $('#eventList').append(loadEvents(events, snap.key()));
-});
-
-
 // Adding New Event
 $('#eventRegisterSubmit').on("click", function() {
   if ($('#eventName').val() !== '') {
     Events
       .push({
         eventName: $('#eventName').val(),
-        eventDate: $('#eventDate').val(),
-        eventEmail: $('#eventEmail').val()
+        eventDate: $('#eventDate').val()        
       });
   } else {
     window.alert('Please enter event name!');
   }
 });
 
+// fetching event data from the firebase
+Events.on("child_added", function(snap) {
+  var events = snap.val();
+  // console.log("all events", snap.key(), snap.val());
+  $('#eventList').append(loadEvents(events, snap.key()));
+});
+
+// Displaying events on the table
+var loadEvents = function(event, id) {
+  console.log(event);
+  if (event) {
+    var html = '';
+    html += '<tr class=" eventitem">';
+    html += '<td>' + event.eventName + '</td>';
+    html += '<td>' + event.eventDate + '</td>'; 
+    html += '<td></td>';    
+    html += '<td>';
+    html += '<button type="button" class="btn btn-primary" id ="' + id + '" onclick="fetchAttendees(' + "id" + ')" data-toggle="modal" data-tooltip="true" data-placement="bottom" title="View Attendees." data-target= "#todayModal" id="viewregister">';
+    html += "View Attendees";
+    html += '</button>  ';
+    html += '<button type="button" class="btn btn-primary" onclick="getId(' + "id" + ')" data-toggle="modal" data-tooltip = "true" data-placement="bottom" title="Check-In" data-target= "#checkInModal" id="' + id + '">Add Attendee</i>';
+    html += '</button>  ';
+    html += '</td>';
+    html += '</tr>';
+    return html;
+  }
+};
+
 // Adding New Attendee
-$('#checkinSubmit').on("click", function() {
+$('#checkinSubmit').on("click", function(e) {
+  e.preventDefault();
   console.log(eventId);
   if ($('#firstName').val() !== '') {
     Events.child(eventId).child('attendees')
@@ -73,33 +70,37 @@ $('#checkinSubmit').on("click", function() {
         lastname: $('#lastName').val(),
         email: $('#email').val()
       });
+      attendeeForm.reset();
   } else {
     window.alert('Please enter attendee name!');
   }
 });
 
+// fetching attendees' data from the firebase
+var fetchAttendees = function(id) {
+  Events.child(id).child('attendees').on("child_added", function(snap) {
+    //console.log("added", snap.key(), snap.val());
+    $('#attendeeList').append(loadAttendees(snap.val()));
 
-
-// fetching event data from the firebase
-var loadEvents = function(event, id) {
-  console.log(event);
-  if (event) {
-    var html = '';
-    html += '<tr class=" eventitem">';
-    html += '<td>' + event.eventName + '</td>';
-    html += '<td>' + event.eventDate + '</td>';
-    html += '<td>' + event.eventName + '</td>';
-    html += '<td>';
-    html += '<button type="button" class="btn btn-primary" id ="' + id + '" onclick="fetchAttendees(' + "id" + ')" data-toggle="modal" data-tooltip="true" data-placement="bottom" title="Add Attendees." data-target= "#todayModal" id="viewregister">';
-    html += "View Today's Register";
-    html += '</button>  ';
-    html += '<button type="button" class="btn btn-primary" onclick="getId(' + "id" + ')" data-toggle="modal" data-tooltip = "true" data-placement="bottom" title="Check-In" data-target= "#checkInModal" id="' + id + '"><i class="fa fa-check"></i>';
-    html += '</button>  ';
-    html += '</td>';
-    html += '</tr>';
-    return html;
-  }
+  });
 };
+
+// fetches the event id 
+function getId(id) {
+  eventId = id;
+}
+
+// Displaying events on the table
+function loadAttendees(attendee) {
+  console.log(attendee);
+  var html = '';
+  html += '<tr class=" eventitem">';
+  html += '<td>' + attendee.firstname + '</td>';
+  html += '<td>' + attendee.lastname + '</td>';
+  html += '<td>' + attendee.email + '</td>';
+  html += '</tr>';
+  return html;
+}
 
 // Logging out of the system
 
@@ -108,35 +109,6 @@ $('#logout').on("click", function log_out() {
   window.location.assign("/");
 
 });
-
-
-// all the function go here
-
-//prepare attendee object's HTML
-function loadAttendees(attendee) {
-  console.log(attendee);
-  var html = '';
-  html += '<tr class=" eventitem">';
-  html += '<td>' + attendee.firstname + '</td>';
-  html += '<td>' + attendee.lastname + '</td>';
-  html += '<td>' + attendee.email + '</td>';
-
-  html += '</tr>';
-  return html;
-}
-
-function getId(id) {
-  eventId = id;
-}
-
-// // load older attendees as well as any newly added one...
-var fetchAttendees = function(id) {
-  Events.child(id).child('attendees').on("child_added", function(snap) {
-    //console.log("added", snap.key(), snap.val());
-    $('#attendeeList').append(loadAttendees(snap.val()));
-
-  });
-};
 
 var auth = function() {
   if (!window.localStorage.getItem('firebase:session::easyattendance')) {
